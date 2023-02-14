cc8378bec729e6773b14e3ec896b03a907ad80f0
Marty <maruri2424@gmail.com>
2023-02-13 20:02:43 -0800

    hide config

--- src/components/utils/firebase.js
+++ src/components/utils/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCwFGH-xMwdtEX4HaX8kSWMZCZdPZOFJIo",
-  authDomain: "wheres-waldo-4ce5e.firebaseapp.com",
-  projectId: "wheres-waldo-4ce5e",
-  storageBucket: "wheres-waldo-4ce5e.appspot.com",
-  messagingSenderId: "892984309195",
-  appId: "1:892984309195:web:3006dc83e21f5e8105724e"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
